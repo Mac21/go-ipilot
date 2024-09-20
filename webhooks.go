a7886ca887f856285987c44ccc7057b606f9a6c3
Mac21 <russ.tolle@gmail.com>
2024-09-20 10:14:28 -0600

    Add Event type for webhooks

--- webhooks.go
+++ webhooks.go
@@ -1,1 +1,19 @@
 package ipilot
+
+type Event struct {
+	AccountID                 int    `json:"account_id"`
+	Object                    string `json:"event_object"`
+	Action                    string `json:"event_action"`
+	Type                      string `json:"event_type"`
+	OrderType                 string `json:"order_type"`
+	Module                    string `json:"module"`
+	PortCount                 int    `json:"port_count"`
+	NumberCount               int    `json:"number_count"`
+	NumberAssigned            int    `json:"number_assigned"`
+	CallpathCount             int    `json:"call_path_count"`
+	PONNumber                 string `json:"pon_number"`
+	ExternalCustomerAccountID string `json:"external_customer_account_id"`
+	ResellerOwnSKU            string `json:"reseller_own_sku"`
+	PlatformOwnSKU            string `json:"platform_own_sku"`
+	SystemSKU                 string `json:"system_sku"`
+}
